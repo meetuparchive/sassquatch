fe3e6d52287411dd0397e85d7174fb14974be1e7
Adam Detrick <akdetrick@gmail.com>
2013-09-10 17:21:07 -0400

    label rgb and hex with JS

--- _site/assets/js/colors.js
+++ _site/assets/js/colors.js
@@ -1,30 +1,31 @@
 $(function() {
 	var $meetupSwatches = $('td.swatch'),
 		$thirdPartySwatches = $('#thirdPartyColors dl');
-		
-	function componentToHex(c) {
-		var hex = c.toString(16);
-		return hex.length == 1 ? "0" + hex : hex;
-	}
 
-	function cssRgbToHex( cssVal ) {
-		var RGB = [], parts = cssVal.split(',');
-		for (i=0;i<parts.length;i++) {
-			RGB.push(parts[i].match(/[0-9]+/)[0]);
+	function rgbToHex(color) {
+		if (color.substr(0, 1) === '#') {
+			return color;
 		}
-		return "#" + componentToHex(RGB[0]) + componentToHex(RGB[1]) + componentToHex(RGB[2]);
+		var digits = /(.*?)rgb\((\d+), (\d+), (\d+)\)/.exec(color);
+
+		var red = parseInt(digits[2]);
+		var green = parseInt(digits[3]);
+		var blue = parseInt(digits[4]);
+
+		var rgb = blue | (green << 8) | (red << 16);
+		return digits[1] + '#' + rgb.toString(16);
 	};
 
 	$meetupSwatches.each(function(i, td) {
 		var rgb = getComputedStyle(td).backgroundColor;
-		$(td).text(rgb);
+		$(td).html('<div class="small">' + rgb + '</div><div class="small">' + rgbToHex(rgb) + '</div>');
 	});
 
 	$thirdPartySwatches.each(function(i, dl) {
 		var $dl = $(dl),
 			rgb = getComputedStyle($dl.find('dt')[0]).backgroundColor;
 
-		$dl.find('dd.hex').text(rgb);
+		$dl.find('dd.hex').html('<div class="small">' + rgb + '</div><div class="small">' + rgbToHex(rgb) + '</div>');
 	});
 
 });
