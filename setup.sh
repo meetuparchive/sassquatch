789525bdea933243fd989b3b355a7c39df4aa1e2
Adam Detrick <akdetrick@gmail.com>
2013-10-13 12:04:19 -0400

    stub update script

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "~~~~~~~~ getting gem system latest..."
+gem update --system
+
+echo "~~~~~~~~ getting latest for bundler gem..."
+gem install bundler
+
+echo "~~~~~~~~ installing required gems..."
+bundle install
+
+echo "~~~~~~~~ installing python requirements..."
+pip install -r requirements.txt
+
+echo "~~~~~~~~ recompile sassquatch..."
+rake compile
+
+exit 0
