1135570bbe8806ee2e9642f13c7105372caca350
Adam Detrick <akdetrick@gmail.com>
2013-11-05 18:38:53 -0500

    fixes #76

--- setup.sh
+++ setup.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-echo "~~~~~~~~ getting gem system latest..."
-gem update --system
+#echo "~~~~~~~~ getting gem system latest..."
+#gem update --system
 
-echo "~~~~~~~~ getting latest for bundler gem..."
-gem install bundler
+#echo "~~~~~~~~ getting latest for bundler gem..."
+#gem install bundler
 
 echo "~~~~~~~~ installing required gems..."
 bundle install
 
 echo "~~~~~~~~ installing python requirements..."
 pip install -r requirements.txt
+
+echo "~~~~~~~~ installing bower components..."
+bower install
 
 echo "~~~~~~~~ recompile sassquatch..."
 rake compile
