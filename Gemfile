117db7894762b90dbd1e295bd01a33d8603675aa
Adam Detrick <akdetrick@gmail.com>
2014-01-23 15:30:06 -0500

    add compass

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem "rake", "~> 10.1.0"
-gem "sass", "=3.2.6"
-gem "compass", "~> 0.12.2"
+gem "compass", "=0.12.2"
+gem "sass", "=3.2.10"
+gem "breakpoint", "=2.0.7"
 gem "jekyll", "=1.4.2"
---
+++ config.rb
@@ -0,0 +1,15 @@
+require 'compass/import-once/activate'
+
+# Require any additional compass plugins here.
+require 'breakpoint'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+
+output_style = :compressed
+
+# line_comments = false
