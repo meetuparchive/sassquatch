495ae7de89729b1603e0695d57b3d4af3ae3ac11
Adam Detrick <akdetrick@gmail.com>
2015-11-11 15:28:09 -0500

    add components

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+gem "sass", "=3.4.4"
+
+group :development do
+	gem 'hologram'
+end
---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "~~~~~~~~ using correct ruby..."
+rbenv install
+
+echo "~~~~~~~~ getting gem system latest..."
+gem update --system
+
+echo "~~~~~~~~ getting latest for bundler gem..."
+gem install bundler
+
+echo "~~~~~~~~ installing required gems..."
+bundle install
+
+echo "~~~~~~~~ installing required node modules..."
+npm install
+
+echo "~~~~~~~~ installing bower components..."
+bower install
+
+echo "~~~~~~~~ recompile sassquatch..."
+grunt
+
+exit 0
